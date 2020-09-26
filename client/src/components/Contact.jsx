34ab9c71129c80686433db4453811277163c6147
shailendrarijal <shailendrarijal@gmail.com>
2020-09-26 21:39:15 +1000

    'update-email'

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Contact() {
   
+  const [visibility, setVisibility] = useState(false);
+
+    function showInfo() {
+        setVisibility(!visibility);
+    }
+
   return (
     <div>
         <section id="contact" class="contact text-center">
@@ -12,9 +18,12 @@ function Contact() {
                 <a class="contact-item mr-5" target="_blank" href="https://github.com/shailendrarijal">GitHub</a>
                 
                 <a class="contact-item mr-5" href="https://www.freecodecamp.org/shailendrarijal" target="_blank">FreeCodeCamp</a>
-                <p id="btn-email" class="contact-item mr-5">Email</p>
+                <p id="btn-email" class="contact-item mr-5" onClick={showInfo}>Email</p>
                 </div>
             </div>
+
+            {visibility? <div className={visibility, "text-center container"}><span>Email: shailendrarijal@gmail.com</span></div> : null }
+
         </section>
     </div>
   );
