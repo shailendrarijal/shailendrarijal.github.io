571bd8f9825a9e7b618cca3d3c8988a6367d3a9c
shailendra <shailendra@agures.com>
2023-03-14 13:41:28 +1100

    fix style

--- src/components/Kasturighar/Kasturighar.jsx
+++ src/components/Kasturighar/Kasturighar.jsx
@@ -85,25 +85,25 @@ function Kasturighar() {
                     <h2>Get In Touch</h2>
                     <div class="row">
                         <div class="col-lg-3 col-md-3 col-sm-4">
-                            <a href="https://www.facebook.com/kasturighar" target="_blank"><div id="facebook-icon-container" class="icon-container">
+                            <a href="https://www.facebook.com/kasturighar" target="_blank"><div id="facebook-icon-container" class="icon-container-kasturighar">
                                 <img class="icon" src={iconFacebook} alt="facebook link" />
                                 <span>Facebook</span>
                             </div></a>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-4">
-                            <a href="https://www.instagram.com/kasturi.ghar/" target="_blank"><div id="instagram-icon-container" class="icon-container">
+                            <a href="https://www.instagram.com/kasturi.ghar/" target="_blank"><div id="instagram-icon-container" class="icon-container-kasturighar">
                                 <img class="icon" src={iconInstagram} alt="instagram link" />
                                 <span>Instagram</span>
                             </div></a>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-4">
-                            <div id="phone-icon-container" class="icon-container" onClick={showInfo}>
+                            <div id="phone-icon-container" class="icon-container-kasturighar" onClick={showInfo}>
                                 <img class="icon" src={iconPhone} alt="phone link" />
                                 <span id="btn-phone" class="contact-item mr-5">Phone</span>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-4">
-                            <div id="email-icon-container" class="icon-container" onClick={showInfo}>
+                            <div id="email-icon-container" class="icon-container-kasturighar" onClick={showInfo}>
                                 <img class="icon" src={iconEmail} alt="email link" />
                                 <span id="btn-email" class="contact-item mr-5">Email</span>
                             </div>
