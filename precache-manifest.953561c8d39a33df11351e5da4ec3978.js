2f70b4bbb6dcc4d6be161931691cca8df0994432
shailendra <shailendra@agures.com>
2023-03-14 12:12:14 +1100

    Updates

---
+++ precache-manifest.953561c8d39a33df11351e5da4ec3978.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac961dcaf3daa0c56ae38fc5c979320",
+    "url": "/shailendrarijal.github.io/index.html"
+  },
+  {
+    "revision": "35b8b23529651f4a9c4e",
+    "url": "/shailendrarijal.github.io/static/css/main.f79d4247.chunk.css"
+  },
+  {
+    "revision": "7fbec3ce31512c2efa3a",
+    "url": "/shailendrarijal.github.io/static/js/2.94ce8749.chunk.js"
+  },
+  {
+    "revision": "85172917a179453ae1ec2cbf75a8b31c",
+    "url": "/shailendrarijal.github.io/static/js/2.94ce8749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e342d996bbea13464b6",
+    "url": "/shailendrarijal.github.io/static/js/3.d88ac536.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/shailendrarijal.github.io/static/js/3.d88ac536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b8b23529651f4a9c4e",
+    "url": "/shailendrarijal.github.io/static/js/main.45a7affb.chunk.js"
+  },
+  {
+    "revision": "5664e0ae3c05ca2cf2aa",
+    "url": "/shailendrarijal.github.io/static/js/runtime-main.cc245c8c.js"
+  },
+  {
+    "revision": "78eee27287bdc9c941d8f5457e3c199d",
+    "url": "/shailendrarijal.github.io/static/media/about-image.78eee272.jpg"
+  },
+  {
+    "revision": "a9eed18bda5b095be20ba02d0b83a9e0",
+    "url": "/shailendrarijal.github.io/static/media/backgroundHome.a9eed18b.jpg"
+  },
+  {
+    "revision": "f1541439ecf3b6dfb7a458326461b1ad",
+    "url": "/shailendrarijal.github.io/static/media/bbn.f1541439.JPG"
+  },
+  {
+    "revision": "9dfbe912b8c2ee8663f3072f39264eec",
+    "url": "/shailendrarijal.github.io/static/media/collage.9dfbe912.png"
+  },
+  {
+    "revision": "2054f10b447e2c64d41f7769c98a960c",
+    "url": "/shailendrarijal.github.io/static/media/crash.2054f10b.mp3"
+  },
+  {
+    "revision": "ceb8e23476555e636282d24dd8f087c6",
+    "url": "/shailendrarijal.github.io/static/media/crash.ceb8e234.png"
+  },
+  {
+    "revision": "89365cc93eca0dbecb7bb64835c30ecc",
+    "url": "/shailendrarijal.github.io/static/media/kasturighar.89365cc9.JPG"
+  },
+  {
+    "revision": "ba702fe1bc54cb13d79df5a492fe0096",
+    "url": "/shailendrarijal.github.io/static/media/kick-bass.ba702fe1.mp3"
+  },
+  {
+    "revision": "2cec27c0a03ec045a907336cf8afbcf1",
+    "url": "/shailendrarijal.github.io/static/media/kick.2cec27c0.png"
+  },
+  {
+    "revision": "7c5399ce387ad8c3eb3fc206dd4f0b18",
+    "url": "/shailendrarijal.github.io/static/media/product1.7c5399ce.jpg"
+  },
+  {
+    "revision": "4cfafca3a73b1076dbdceec1d72155a7",
+    "url": "/shailendrarijal.github.io/static/media/product2.4cfafca3.jpeg"
+  },
+  {
+    "revision": "6b14597604e2c8450f2c594807310d33",
+    "url": "/shailendrarijal.github.io/static/media/product3.6b145976.jpeg"
+  },
+  {
+    "revision": "422e34fafff0856c2ac62bdcaaba972c",
+    "url": "/shailendrarijal.github.io/static/media/product4.422e34fa.jpg"
+  },
+  {
+    "revision": "3b0c9da5d5d2ee85c7d1aa27e5e026a2",
+    "url": "/shailendrarijal.github.io/static/media/scroll1.3b0c9da5.png"
+  },
+  {
+    "revision": "425a4958924bf354baa4410cb383a6f9",
+    "url": "/shailendrarijal.github.io/static/media/scroll2.425a4958.png"
+  },
+  {
+    "revision": "bd8be2acb726d3059b08d59d681d1305",
+    "url": "/shailendrarijal.github.io/static/media/scroll3.bd8be2ac.png"
+  },
+  {
+    "revision": "71d516cdaf3b6f9b809950701f268c46",
+    "url": "/shailendrarijal.github.io/static/media/scroll4.71d516cd.png"
+  },
+  {
+    "revision": "fcea7bf1fc78b4c13e4054d132dc3df8",
+    "url": "/shailendrarijal.github.io/static/media/snare.fcea7bf1.mp3"
+  },
+  {
+    "revision": "fd3ed0a24893a6ba1ecd41f4953347f0",
+    "url": "/shailendrarijal.github.io/static/media/snare.fd3ed0a2.png"
+  },
+  {
+    "revision": "52c69e95fc22cd43caff7e2600f8c7dc",
+    "url": "/shailendrarijal.github.io/static/media/survey-form.52c69e95.JPG"
+  },
+  {
+    "revision": "5ea394183902350bc75d78746e7a373a",
+    "url": "/shailendrarijal.github.io/static/media/technical-documentation.5ea39418.JPG"
+  },
+  {
+    "revision": "d629302b86a5f481adcd2d06cdbba502",
+    "url": "/shailendrarijal.github.io/static/media/testimonials.d629302b.jpg"
+  },
+  {
+    "revision": "d6bb6a857285a3d8a4764f6e4db623d0",
+    "url": "/shailendrarijal.github.io/static/media/tom-1.d6bb6a85.mp3"
+  },
+  {
+    "revision": "1d98c4ba7f9854809b5d9fb7cc5e0144",
+    "url": "/shailendrarijal.github.io/static/media/tom-2.1d98c4ba.mp3"
+  },
+  {
+    "revision": "5745a1059e0444d06fc6a955ca73c247",
+    "url": "/shailendrarijal.github.io/static/media/tom-3.5745a105.mp3"
+  },
+  {
+    "revision": "a1d2e98759080c917c741e65404d684c",
+    "url": "/shailendrarijal.github.io/static/media/tom-4.a1d2e987.mp3"
+  },
+  {
+    "revision": "88e4de6e7f75b90cb38d025898670175",
+    "url": "/shailendrarijal.github.io/static/media/tom1.88e4de6e.png"
+  },
+  {
+    "revision": "028ef6cad97e5b9ab573c7a47d1f79a6",
+    "url": "/shailendrarijal.github.io/static/media/tom2.028ef6ca.png"
+  },
+  {
+    "revision": "b799f9dd18e627705bf779db2fb84302",
+    "url": "/shailendrarijal.github.io/static/media/tom3.b799f9dd.png"
+  },
+  {
+    "revision": "6c595733f604da67fb3fb5c403d9ddb6",
+    "url": "/shailendrarijal.github.io/static/media/tom4.6c595733.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/shailendrarijal.github.io/precache-manifest.953561c8d39a33df11351e5da4ec3978.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shailendrarijal.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
